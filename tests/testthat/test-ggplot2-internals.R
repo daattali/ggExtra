64718648ee4accf6c32ecef7e62c45052eac5daf
Chris Baker <chriscrewbaker@gmail.com>
2017-04-20 20:26:40 -0400

    Add unit tests for ggplot2 internals (title accession only)

---
+++ tests/testthat/test-ggplot2-internals.R
@@ -0,0 +1,38 @@
+runggplot2InternalsTests <- function(ggplot2Version) {
+  
+  context <- paste("ggplot2 internals under ggplot2 version", ggplot2Version)
+  
+  context(context)
+  
+  test_that("ggExtra's accession of ggplot2 title grobs works" , {
+    
+    titleP <- function(title) {
+      basicScatP() + title
+    }
+    titleList <- list(
+      noSub = ggplot2::ggtitle("hi"), 
+      sub = ggplot2::ggtitle("there", subtitle = "friend")
+    )
+    
+    expect_true({
+      gTest <- sapply(titleList, function(x) {
+        "grob" %in% class(ggExtra:::getTitleGrob(titleP(x)))
+        })
+      sum(gTest) == 2
+    })
+    expect_true({
+      gTest <- sapply(titleList, function(x) {
+        !is.null(ggplot2::ggplot_build(titleP(x))$plot$labels$title)
+      })
+      sum(gTest) == 2
+    })
+
+    expect_true({
+      is.null(ggplot2::ggplot_build(titleP(ggplot2::theme()))$plot$labels$title)
+    })
+    
+  })
+
+}
+
+runggplot2InternalsTests("2.2.1")
