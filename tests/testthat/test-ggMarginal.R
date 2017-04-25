c976e2b1ff30d3831dce9939b849ae6351c7d174
Chris Baker <chriscrewbaker@gmail.com>
2017-04-25 12:44:10 -0400

    Add line breaks to comment

--- tests/testthat/test-ggMarginal.R
+++ tests/testthat/test-ggMarginal.R
@@ -1,4 +1,5 @@
-# Wrap up the ggMarginal visual tests in a function runMarginalTests so that it's easy to test under multiple versions of ggplot2
+# Wrap up the ggMarginal visual tests in a function runMarginalTests so that 
+# it's easy to test under multiple versions of ggplot2
 runMarginalTests <- function(ggplot2Version) {
   
   context <- paste("ggMarginal under ggplot2 version", ggplot2Version)
