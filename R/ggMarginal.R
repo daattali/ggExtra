d42236851f78109aae4553e0c7914b1df83ad198
Chris Baker <chriscrewbaker@gmail.com>
2017-09-21 19:40:38 -0400

    Use geom_density for marginal plots that are density plots

--- R/ggMarginal.R
+++ R/ggMarginal.R
@@ -89,14 +89,9 @@ ggMarginal <- function(p, data, x, y, type = c("density", "histogram", "boxplot"
 
   # Fill in param defaults and consolidate params into single list (prmL).
   prmL <- toParamList(exPrm = list(...), xPrm = xparams, yPrm = yparams)
-
-  # After ggplot2 v1.0.1, layers became strict about parameters
-  if (type == "density") {
-    prmL[['exPrm']][['fill']] <- NULL
-  }
-
-  # Create one version of the scat plot (scatP), based on values of p, data, x,
-  # and y...also remove all margin around plot so that it's easier to position
+  
+  # Create one version of the scat plot (scatP), based on values of p, data, x, 
+  # and y...also remove all margin around plot so that it's easier to position 
   # the density plots beside the main plot
   scatP <- reconcileScatPlot(p = p, data = data, x = x, y = y) +
     ggplot2::theme(plot.margin = grid::unit(c(0, 0, .25, .25), "cm"))
