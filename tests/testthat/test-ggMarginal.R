29a2dad6352f061318fe3012ff710c50ebbebf57
Chris Baker <chriscrewbaker@gmail.com>
2017-09-21 19:36:52 -0400

    Move checks on enviornment (for seeing whether to run tests) into function

--- tests/testthat/test-ggMarginal.R
+++ tests/testthat/test-ggMarginal.R
@@ -1,21 +1,8 @@
-## By default, do not run the tests
-## which also means do not run on CRAN
-runTests <- FALSE
-## Use the Travis / GitHub integrations as we set this
-## environment variable to "yes" in .travis.yml
-if (Sys.getenv("RunGgplot2Tests") == "yes") runTests <- TRUE
-## Also run the tests when building on Dean's machine
-if (isTRUE(unname(Sys.info()["user"]) == "Dean")) runTests <- TRUE
+runMarginalTests <- function(ggplot2Version) {
 
-if (runTests) {
+  context <- paste("ggMarginal under ggplot2 version", ggplot2Version)
 
-  # Wrap up the ggMarginal visual tests in a function runMarginalTests so that
-  # it's easy to test under multiple versions of ggplot2
-  runMarginalTests <- function(ggplot2Version) {
-
-    context <- paste("ggMarginal under ggplot2 version", ggplot2Version)
-
-    context(context)
+  context(context)
 
     test_that("ggMarginal can produce basic marginal plots" , {
       sapply(c("basic density", "basic histogram", "basic boxplot", "basic violin",
@@ -36,16 +23,17 @@ if (runTests) {
                  expectDopp2(funName = x, ggplot2Version = ggplot2Version))
     })
 
-  }
+}
 
-  # Function to run all visual regression tests across all ggplot2 versions
-  runMarginalTestsApply <- function(ggplot2Versions) {
-    sapply(ggplot2Versions, function(ggplot2Version) {
-      withGGplot2Version(ggplot2Version, {
-        runMarginalTests(ggplot2Version)
-      })
+# Function to run all visual regression tests across all ggplot2 versions
+runMarginalTestsApply <- function(ggplot2Versions) {
+  sapply(ggplot2Versions, function(ggplot2Version) {
+    withGGplot2Version(ggplot2Version, {
+      runMarginalTests(ggplot2Version)
     })
-  }
+  })
+}
 
+if (shouldTest()) {
   runMarginalTestsApply(c("2.2.0", "2.2.1", "latest"))
 }
--- tests/testthat/test-ggplot2-internals.R
+++ tests/testthat/test-ggplot2-internals.R
@@ -1,47 +1,34 @@
-## By default, do not run the tests
-## which also means do not run on CRAN
-runTests <- FALSE
-## Use the Travis / GitHub integrations as we set this
-## environment variable to "yes" in .travis.yml
-if (Sys.getenv("RunGgplot2Tests") == "yes") runTests <- TRUE
-## Also run the tests when building on Dean's machine
-if (isTRUE(unname(Sys.info()["user"]) == "Dean")) runTests <- TRUE
+runggplot2InternalsTests <- function(ggplot2Version) {
 
-if (runTests) {
+  context <- paste("ggplot2 internals under ggplot2 version", ggplot2Version)
 
-  runggplot2InternalsTests <- function(ggplot2Version) {
+  context(context)
 
-    context <- paste("ggplot2 internals under ggplot2 version", ggplot2Version)
+  test_that("ggExtra's accession of ggplot2 title grobs works" , {
 
-    context(context)
+    titleP <- function(title) {
+      basicScatP() + title
+    }
+    titleList <- list(
+      noSub = ggplot2::ggtitle("hi"),
+      sub = ggplot2::ggtitle("there", subtitle = "friend")
+    )
 
-    test_that("ggExtra's accession of ggplot2 title grobs works" , {
-
-      titleP <- function(title) {
-        basicScatP() + title
-      }
-      titleList <- list(
-        noSub = ggplot2::ggtitle("hi"),
-        sub = ggplot2::ggtitle("there", subtitle = "friend")
-      )
-
-      expect_true({
-        gTest <- sapply(titleList, function(x) {
-          length(ggExtra:::getTitleGrobs(titleP(x))) == 2
-        })
-        all(gTest)
+    expect_true({
+      gTest <- sapply(titleList, function(x) {
+        length(ggExtra:::getTitleGrobs(titleP(x))) == 2
       })
-      expect_true({
-        gTest <- sapply(titleList, function(x) {
-          !is.null(ggplot2::ggplot_build(titleP(x))$plot$labels$title)
-        })
-        all(gTest)
+      all(gTest)
+    })
+    expect_true({
+      gTest <- sapply(titleList, function(x) {
+        !is.null(ggplot2::ggplot_build(titleP(x))$plot$labels$title)
       })
+      all(gTest)
+    })
 
-      expect_true({
-        is.null(ggplot2::ggplot_build(titleP(ggplot2::theme()))$plot$labels$title)
-      })
-
+    expect_true({
+      is.null(ggplot2::ggplot_build(titleP(ggplot2::theme()))$plot$labels$title)
     })
 
     test_that("ggplot2 models scatter plot data as expected" , {
@@ -54,16 +41,19 @@ if (runTests) {
 
     })
 
-  }
+  })
+  
+}
 
-  # Function to run all tests against ggplot2 internals under all ggplot2 versions
-  runInternalTestsApply <- function(ggplot2Versions) {
-    sapply(ggplot2Versions, function(ggplot2Version) {
-      withGGplot2Version(ggplot2Version, {
-        runggplot2InternalsTests(ggplot2Version)
-      })
+# Function to run all tests against ggplot2 internals under all ggplot2 versions
+runInternalTestsApply <- function(ggplot2Versions) {
+  sapply(ggplot2Versions, function(ggplot2Version) {
+    withGGplot2Version(ggplot2Version, {
+      runggplot2InternalsTests(ggplot2Version)
     })
-  }
+  })
+}
 
+if (shouldTest()) {
   runInternalTestsApply(c("2.2.0", "2.2.1", "latest"))
 }
