02f424b9d900658068f461184e2fa548d47cdf11
Chris Baker <chriscrewbaker@gmail.com>
2017-11-07 00:22:51 -0800

    Fix tests (#73)
    * Move checks on enviornment (for seeing whether to run tests) into function
    
    * Fix name of violin test plot (last piece of closing #69)
    
    * Drop newlines at eofs
    
    * Muffle "structure with NULLs" warnings (which happen under ggplot 2.2.0)
    
    * outdent code
    
    * Make sure r uses specific versions of vdiffr, svglite, and fontquiver for visual unit tests (closes #69?)
    
    * add back newlines at eof
    
    * drop debugging lines
    
    * move withVersions for plot-rendering deps to top-level testing statement
    
    * make sure funs inside writeSvg are scoped correctly
    
    * Only use withVersions on ggmarginal tests
    
    * Build/test ggExtra inside docker container
    
    * add after_script command to cat logs
    
    * change dir in which ggExtra.Rcheck dir is looked for
    
    * Persist ggExtra.Rcheck dir outside docker container
    
    * fix persistence of ggExtra.Rcheck dir

--- tests/testthat/helper-funs.R
+++ tests/testthat/helper-funs.R
@@ -40,30 +40,103 @@ funList <-
 
 expectDopp2 <- function(funName, ggplot2Version) {
   path <- paste0("ggMarginal/ggplot2-", ggplot2Version)
-  vdiffr::expect_doppelganger(funName, funList[[funName]](), path = path)
+  vdiffr::expect_doppelganger(
+    funName, 
+    printMuffled(funList[[funName]]()),
+    path = path
+  )
 }
 
-# withGGplot2Version is essentially the same function as with_pkg_version that
+# withVersions is essentially the same function as with_pkg_version that
 # appears here: https://gist.github.com/jimhester/d7aeb95bbed02f2985a87c2a3ede19f5.
-# This function allows us to run unit tests under different versions of ggplot2.
-withGGplot2Version <- function(ggplot2Version, code) {
-  if (isNamespaceLoaded("ggplot2")) {
-    unloadNamespace("ggplot2")
-  }
+# This function allows us to run unit tests under different versions of ggplot2,
+# confirming that ggMarginal works under all versions >= 2.2.0. We also set
+# the package versions of three packages (vdiffr, fontquiver, and svglite) 
+# that could slightly effect the rendering of the SVGs, thus causing the tests 
+# to fail.
+withVersions <- function(..., code) {
+  
+  packageVersions <- list(...)
+  
+  unloadPackages(packages = names(packageVersions))
+  on.exit(unloadPackages(packages = names(packageVersions)))
+  
   dir <- tempfile()
   dir.create(dir)
   on.exit(unlink(dir))
-  withr::with_libpaths(dir, action = "prefix", {
-    on.exit(unloadNamespace("ggplot2"))
-
-    if (ggplot2Version == "latest") {
-      devtools::install_github("tidyverse/ggplot2")
-    } else {
-      devtools::install_version("ggplot2", ggplot2Version, quiet = TRUE,
-                                repos = "https://cloud.r-project.org",
-                                type = "source")
-    }
-
+  
+  withr::with_libpaths(dir, action = "prefix", code = {
+    mapply(installVersion2, package = names(packageVersions), version = packageVersions)
     force(code)
+  })
+}
+
+unloadPackages <- function(packages) {
+  lapply(packages, function(x) {
+    if (isNamespaceLoaded(x)) {
+      unloadNamespace(x)
+    }
+  })
+}
+
+installVersion2 <- function(package, version) {
+  
+  currentVersion <- tryCatch(
+    utils::packageVersion(package),
+    error = function(e) ""
+  )
+  
+  if (package == "ggplot2" && version == "latest") {
+    devtools::install_github("tidyverse/ggplot2", upgrade_dependencies = FALSE)
+  } else if (currentVersion != version) {
+    repos <- getSnapShotRepo(package = package, version = version)
+    devtools::install_version(package, version, repos = repos)
+  } else {
+    return()
+  }
+}
+
+getSnapShotRepo <- function(package, version) {
+  tryCatch(
+    attemptRepoDate(package = package, version = version),
+    error = function(e) "https://cloud.r-project.org"
+  )
+}
+
+attemptRepoDate <- function(package, version) {
+  arch <- devtools:::package_find_repo(
+    package = package, repo = "https://cloud.r-project.org"
+  )
+  versions <- gsub(".*/[^_]+_([^[:alpha:]]+)\\.tar\\.gz", "\\1", arch$path)
+  date <- arch[versions == version, "mtime", drop = TRUE]
+  dateString <- as.character(as.Date(date, format = "%Y/%m/%d") + 2)
+  sprintf("https://mran.microsoft.com/snapshot/%s", dateString)
+}
+
+## By default, do not run the tests (which also means do not run on CRAN)
+shouldTest <- function() {
+  if (
+    ## Use the Travis / GitHub integrations as we set this environment variable 
+    ## to "yes" in .travis.yml
+    Sys.getenv("RunGgplot2Tests") == "yes" || 
+    ## Also run the tests when building on Dean or Chris' machine
+    Sys.info()["user"] %in% c("cbaker", "Dean")
+  ) {
+    TRUE
+  } else {
+    FALSE
+  }
+}
+
+# Misc function to drop muffle a particular warning that occurs whenever a 
+# ggplot2 plot is printed under version 2.2.0...This warning clogs up the 
+# output of devtools::test(), because we test under ggplot 2.2.0.
+printMuffled <- function(plot) {
+  withCallingHandlers({
+    print(plot)
+  }, warning = function(w) {
+    if (grepl("structure", w, ignore.case = TRUE)) {
+      invokeRestart("muffleWarning")
+    }
   })
 }
