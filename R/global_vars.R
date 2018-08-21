f2a04df64d47e0472b5fffc127af8aff5e16a4c5
Indrajeet Patil <patilindrajeet.science@gmail.com>
2018-08-20 20:19:13 -0400

    Create global_vars.R (#130)

---
+++ R/global_vars.R
@@ -0,0 +1,6 @@
+# defining global variables and functions to appease R CMD Check
+
+utils::globalVariables(names = c(".",
+                                 "..density.."),
+                       package = "ggExtra",
+                       add = FALSE)
