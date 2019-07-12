2f6ed07a42857a88c6da4bb6997e93cf46eb79bb
Chris Baker <chriscrewbaker@gmail.com>
2019-07-12 19:28:59 -0400

    Test gg marginal under ggplot2 v3.2.0 (#146)

--- tests/testthat/helper-funs.R
+++ tests/testthat/helper-funs.R
@@ -203,4 +203,6 @@ printMuffled <- function(plot) {
   })
 }
 
-ggplot2Versions <- c("2.2.0", "2.2.1", "3.0.0", "3.1.0", "3.1.1", "latest")
+ggplot2Versions <- c(
+  "2.2.0", "2.2.1", "3.0.0", "3.1.0", "3.1.1", "3.2.0", "latest"
+)
