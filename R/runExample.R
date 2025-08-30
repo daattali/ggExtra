1b32d74be21991849631072a20bf18568393ad06
Dean Attali <daattali@gmail.com>
2025-08-30 09:11:18 -0400

    docs: better docs about deprecated runExample

--- R/runExample.R
+++ R/runExample.R
@@ -1,10 +1,14 @@
-#' Run ggExtra example
+#' Run ggExtra example [DEPRECATED]
 #'
 #' Launch a Shiny app that shows a demo of what can be done with
 #' \code{ggExtra::ggMarginal}.
 #'
 #' This example is also
-#' \href{https://daattali.com/shiny/ggExtra-ggMarginal-demo/}{available online}.
+#' \href{https://daattali.com/shiny/ggExtra-ggMarginal-demo/}{available online}.\cr\cr
+#' \strong{Deprecation Notice:} This function is no longer required since Shiny version
+#' 1.8.1 (March 2024). This function will be removed in a future release of \{ggExtra\}.
+#' You can use \code{shiny::runExample("ggMarginal", package = "ggExtra")} instead of
+#' \code{ggExtra::runExample()}.
 #'
 #' @examples
 #' ## Only run this example in interactive R sessions
@@ -13,7 +17,9 @@
 #' }
 #' @export
 runExample <- function() {
-  appDir <- system.file("examples", "ggMarginal", package = "ggExtra")
+  message("WARNING: `ggExtra::runExample()` is deprecated. Please upgrade to {shiny} version 1.8.1 ",
+          "and use `shiny::runExample(\"ggMarginal\", package = \"ggExtra\")` instead.\n")
+  appDir <- system.file("examples-shiny", "ggMarginal", package = "ggExtra")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `ggExtra`.",
       call. = FALSE
